246161838b27a5c624787f53c779628e40cfa919
Mohamed-Elsisi <elsisi901@gmail.com>
2021-04-19 02:38:15 +0200

    add logs

--- src/main/java/com/graduation/demo/controller/DoctorController.java
+++ src/main/java/com/graduation/demo/controller/DoctorController.java
@@ -5,6 +5,8 @@ import com.graduation.demo.dto.doctorDto;
 import com.graduation.demo.service.DoctorServiceImpl;
 import com.sun.org.slf4j.internal.LoggerFactory;
 import lombok.extern.flogger.Flogger;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,50 +15,57 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class DoctorController {
-    private static final Logger logger =
-            (Logger) LoggerFactory.getLogger(DoctorController.class);
+
     @Autowired
     private DoctorServiceImpl service;
 
     @PostMapping("/addDoctor")
     public ResponseEntity createDoctor(@RequestBody Doctor doctor) {
+        log.info("create_Doctor_Controller ", doctor);
         Doctor doctor1 = service.saveDoctor(doctor);
-        logger.info("NEW_Doctor_LOG", doctor1.toString());
         return ResponseEntity.status(HttpStatus.CREATED).body(doctor1);
     }
 
 
     @PostMapping("/addAllDoctor")
     public ResponseEntity<List<Doctor>> addAllDoctor(@RequestBody List<Doctor> doctors) {
-         List<Doctor> doctor1 =service.saveDoctors(doctors);
-         return ResponseEntity.ok(doctor1);
+        log.info("create_List_Of_Doctor_Controller "+ doctors);
+        List<Doctor> doctor1 = service.saveDoctors(doctors);
+
+        return ResponseEntity.ok(doctor1);
     }
 
     @GetMapping("/findDoctorById/{id}")
     public ResponseEntity findDoctorById(@PathVariable Long id) {
+        log.info("Find doctor by id in controller with id = " + id);
         Doctor doctor = service.getDoctorById(id);
-        if (doctor== null) return ResponseEntity.notFound().build();
+
+        if (doctor == null) return ResponseEntity.notFound().build();
         else
             return ResponseEntity.status(HttpStatus.FOUND).body(doctor);
     }
 
     @GetMapping("/getAllDoctors")
     public List<Doctor> getAllDoctors() {
+        log.info("Get All Doctors in controller ");
         return service.getAllDoctors();
     }
 
     @GetMapping("/findDoctorByName/{name}")
     public ResponseEntity findDoctorByName(@PathVariable String name) {
+        log.info("Find Doctor by Name in Controller with name = " + name);
         Doctor doctor = service.getDoctorByName(name);
-        if (doctor== null) return ResponseEntity.notFound().build();
+        if (doctor == null) return ResponseEntity.notFound().build();
         else
             return ResponseEntity.status(HttpStatus.FOUND).body(doctor);
     }
 
     @DeleteMapping("/deleteDoctor/{id}")
     public ResponseEntity deleteDoctor(Long id) {
+        log.info("Delete doctor by id in controller with id = " + id);
         boolean status = service.deleteDoctorById(id);
         if (status == false) return ResponseEntity.notFound().build();
 
@@ -65,10 +74,11 @@ public class DoctorController {
 
     @PutMapping("/updateDoctor")
     public ResponseEntity updateDoctor(Doctor doctor) {
+        log.info("Update  doctor  controller with object = " + doctor);
         Doctor doctor1 = service.updateDoctor(doctor);
-        if (doctor1== null) return ResponseEntity.notFound().build();
+        if (doctor1 == null) return ResponseEntity.notFound().build();
         else {
-            logger.info("ORDER_UPDATED_LOG", doctor1.toString());
+            // logger.info("ORDER_UPDATED_LOG", doctor1.toString());
             return ResponseEntity.ok(doctor1);
         }
     }
--- src/main/java/com/graduation/demo/service/DoctorServiceImpl.java
+++ src/main/java/com/graduation/demo/service/DoctorServiceImpl.java
@@ -1,10 +1,14 @@
 package com.graduation.demo.service;
+
 import com.graduation.demo.Dao.DoctorRepository;
 import com.graduation.demo.Model.Doctor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+@Slf4j
 @Service
 public class DoctorServiceImpl implements DoctorService {
     @Autowired
@@ -13,7 +17,14 @@ public class DoctorServiceImpl implements DoctorService {
 
     @Override
     public Doctor saveDoctor(Doctor doctor) {
-        return doctorRepository.save(doctor);
+        log.info("calling Save_Doctor service with Object " + doctor);
+        Doctor existingDoctor = doctorRepository.save(doctor);
+        if (existingDoctor == null)
+            log.warn("Save_Doctor With object  is null ");
+        else
+            log.info("Save_Doctor With object response ");
+        return existingDoctor;
+
     }
 
     @Override
@@ -23,34 +34,67 @@ public class DoctorServiceImpl implements DoctorService {
 
     @Override
     public Doctor getDoctorById(Long id) {
-        return doctorRepository.findById(id).orElse(null);
+        log.info("calling get doctor by id  service with id " + id);
+        Doctor existingDoctor = doctorRepository.findById(id).orElse(null);
+        if (existingDoctor == null)
+            log.warn("get doctor with id is null ");
+        else
+            log.info("get doctor by id response ");
+        return existingDoctor;
+
+
     }
 
     @Override
     public List<Doctor> getAllDoctors() {
-        return doctorRepository.findAll();
+        log.info("calling get All_Doctor service  ");
+        List<Doctor> existingDoctors = doctorRepository.findAll();
+        if (existingDoctors == null)
+            log.warn("get All_Doctor is null ");
+        else
+            log.info("get All_Doctor response ");
+
+        return existingDoctors;
+
     }
 
     @Override
     public Doctor getDoctorByName(String name) {
-        return doctorRepository.getDoctorByName(name);
+        log.info("calling get_Doctor_By_Name service  " + name);
+        Doctor existingDoctor = doctorRepository.getDoctorByName(name);
+        if (existingDoctor == null)
+            log.warn("get Doctor By Name is null ");
+        else
+            log.info("get Doctor By Name response ");
+
+        return existingDoctor;
+
     }
 
     @Override
     public boolean deleteDoctorById(Long id) {
+        log.info("calling delete_Doctor_By_Id service " + id);
         Doctor doctor = doctorRepository.findById(id).orElse(null);
-        if (doctor == null) return false;
-
-        else doctorRepository.deleteById(id);
-        return true;
+        if (doctor == null) {
+            log.warn("delete Doctor By Id is null ");
+            return false;
+        } else {
+            log.info("delete Doctor By Id response ");
+            doctorRepository.deleteById(id);
+            return true;
+        }
     }
 
     @Override
     public Doctor updateDoctor(Doctor doctor) {
+        log.info("calling  update_Doctor service " + doctor);
         Doctor existingDoctor = doctorRepository.findById(doctor.getId()).orElse(null);
-        existingDoctor.setName(doctor.getName());
-        existingDoctor.setPhone(doctor.getPhone());
-        existingDoctor.setAddress(doctor.getAddress());
+        if (existingDoctor == null) log.warn("get Doctor By Name service is null ");
+        else
+            log.info("update Doctor response ");
+            existingDoctor.setName(doctor.getName());
+            existingDoctor.setPhone(doctor.getPhone());
+            existingDoctor.setAddress(doctor.getAddress());
 
         return doctorRepository.save(existingDoctor);
 
