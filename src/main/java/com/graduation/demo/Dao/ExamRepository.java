ef7f281feefed9a9a28e4095c6efc738ddd36f11
Mohamed-Elsisi <elsisi901@gmail.com>
2021-07-04 03:46:49 +0200

    first__

--- src/main/java/com/graduation/demo/Dao/ExamRepository.java
+++ src/main/java/com/graduation/demo/Dao/ExamRepository.java
@@ -2,7 +2,16 @@ package com.graduation.demo.Dao;
 
 import com.graduation.demo.Model.Exam;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface ExamRepository extends JpaRepository<Exam,Long> {
+import java.util.List;
+import java.util.Set;
 
+public interface ExamRepository extends JpaRepository<Exam, Long> {
+
+    @Query(value = "Select  exam.* \n" +
+            "From exam  join student_and_course\n" +
+            "on  exam.course_id = student_and_course.course_id and  student_and_course.student_id= :student_id", nativeQuery = true)
+    Set<Exam> getAllExams(@Param("student_id") Long id);
 }
--- src/main/java/com/graduation/demo/service/ExamService.java
+++ src/main/java/com/graduation/demo/service/ExamService.java
@@ -2,9 +2,14 @@ package com.graduation.demo.service;
 
 import com.graduation.demo.Model.Exam;
 
+import java.util.List;
+import java.util.Set;
+
 public interface ExamService {
     public Exam createExam(Exam exam);
     public Exam updateExam(Exam exam);
     public Exam findExam(String name);
+
+    public Set<Exam> getAllExams(Long id);
 
 }
