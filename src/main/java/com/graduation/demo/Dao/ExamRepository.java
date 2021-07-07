9427018f9668c163e49b08904e9d91740b122450
eslamelkhafagy <eslamelkhafagy@gmail.com>
2021-07-07 11:38:54 +0200

    update query

--- src/main/java/com/graduation/demo/Dao/ExamRepository.java
+++ src/main/java/com/graduation/demo/Dao/ExamRepository.java
@@ -10,6 +10,6 @@ import java.util.Set;
 
 public interface ExamRepository extends JpaRepository<Exam, Long> {
 
-    @Query(value = "SELECT ex.* FROM exam ex INNER JOIN course co ON ex.course_id = co.id INNER JOIN level_and_dep le ON co.department=le.department_id AND co.level=le.level_id INNER JOIN student st ON le.id=st.levai_dep_id WHERE st.id= :student_id", nativeQuery = true)
+    @Query(value = "SELECT ex.* FROM exam ex INNER JOIN course co ON ex.course_id = co.id INNER JOIN level_and_dep le ON co.department_id=le.department_id AND co.level_id=le.level_id INNER JOIN student st ON le.id=st.level_dep_id WHERE st.id= :student_id", nativeQuery = true)
     List<Exam> getAllExams(@Param("student_id") Long id);
 }
