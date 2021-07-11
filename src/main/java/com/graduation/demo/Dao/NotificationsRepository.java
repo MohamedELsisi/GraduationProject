2a324bdb75da1e6745620106b5d627b2f5009219
eslamelkhafagy <eslamelkhafagy@gmail.com>
2021-07-11 18:29:03 +0200

    query fix

--- src/main/java/com/graduation/demo/Dao/NotificationsRepository.java
+++ src/main/java/com/graduation/demo/Dao/NotificationsRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface NotificationsRepository extends JpaRepository<Notifications,Long> {
 
-    @Query(value = "SELECT notifications.* from notifications where read=false AND courseId = :course", nativeQuery = true)
-    List<Notifications> findByCourseId(int courseId);
+    @Query(value = "SELECT noti.* from notifications noti where noti.read=false AND noti.course_id = :course", nativeQuery = true)
+    List<Notifications> findByCourseId(int course);
 
 }
